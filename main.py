9458e74e4e51a8f58cecac82ca422869ac1ed1e8
Simon Dinnage <sdinnage@gmail.com>
2022-12-21 07:45:06 +0000

    Got Input + Output files + Directories working Got CVS reading Got XLS creation

--- main.py
+++ main.py
@@ -3,13 +3,135 @@ import xlsxwriter
 import sys, getopt
 import os.path
 
+#
+################################ Global App Data ###############################
+#
+PARSED = int(0)
+NOT_PARSED = int(-1)
+#NOT_MATCHED = str('MATCH')
+# Excel worksheet row
+
+
 inputfile = ''
 outputfile = ''
 filetype = ''
+row= int(0)
+#
+################################ NW Data ####################################
+#
+
+# Transaction Fields
+Date = int(0)
+Trans = int(1)
+Desc = int(2)
+Pout = int(3)
+Pin = int(4)
+Balance = int(5)
+
+# NW Incoming Transactions
+NW_Incoming_Trans = []
+NW_Incoming_Transfers = []
+
+IC_TRANSFER = int(0)
+
+ic_types_dict = {
+    '070040 18408110':'IC_TRANSFER',
+    'Bank credit SCO GBP CMT AC':'IC_TRANSFER'
+}
 
 #
-# Start of main program
+############################ IG FUNCTIONS #####################################
 #
+
+def build_IG_excel():
+    pass
+def process_IG_csv_row(row):
+    print(row)
+
+#
+########################## NW FUNCTIONS #######################################
+#
+
+def write_row_of_text_to_excel(row, col, text):
+    for item in text:
+        #print("Text: ",item)
+        worksheet.write(row, col, item, bold)
+        col+=1
+    row+=1
+    return(row)
+def write_NW_trans_to_excel(row, col, trans):
+
+    for date, trans, desc, pout, pin, balance in (trans):
+        #print("Transaction",date,trans,desc,pout,pin,balance)
+        col = 0
+        worksheet.write(row, col, date)
+     #  col+=1
+     #  worksheet.write(row, col, trans)
+        col+=1
+        worksheet.write(row, col, desc)
+        col+=1
+        worksheet.write(row, col, pin)
+        row += 1
+    return(row)
+def build_NW_excel():
+    # Add a bold format to use to highlight cells.
+
+    row = 0
+    col = 0
+    row = write_row_of_text_to_excel(row, col,['Incoming Payments'])
+    row = write_row_of_text_to_excel(row,col,['Date','Description','Paid In'])
+    row  = write_NW_trans_to_excel(row, col, NW_Incoming_Trans)
+    row = write_row_of_text_to_excel(row, col, ['Transfer Payments'])
+    row = write_NW_trans_to_excel(row, col, NW_Incoming_Transfers)
+
+def check_dictionary(row, dictionary):
+    try:
+        category = dictionary[row[Desc]]
+        #print('Dictionary Match', category)
+        return(category)
+    except:
+        #print('Dictionary Lookup Failed')
+        return('NOT_MATCHED')
+def process_NW_incoming(row):
+
+    category=check_dictionary(row,ic_types_dict)
+    if(category == 'IC_TRANSFER'):
+        print('Transfer',row)
+        NW_Incoming_Transfers.append(row)
+    else:
+        print('I/C',row)
+        NW_Incoming_Trans.append(row)
+    #print("process_NW_incoming NW_Incoming_Trans",NW_Incoming_Trans)
+
+def process_NW_outgoing(row):
+    pass
+def process_NW_csv_row(row):
+
+    #
+    # Eliminate the header
+    #
+    if(len(row)>0):
+       # print(row[0])
+        if(row[0] == 'Account Name:' or row[0] == 'Account Balance:' or row[0] == 'Available Balance: ' or row[0] == 'Date'):
+            return(NOT_PARSED)
+
+        if(len(row[Pin])>0):
+            # Process Incoming
+            process_NW_incoming(row)
+
+        if(len(row[Pout])>0):
+            # Process Outgoing
+            #print(row)
+            process_NW_outgoing(row)
+
+
+
+    #if(row == 'Account Name:'):
+        #print(row)
+
+#####################################################################################
+#####################################  MAIN #########################################
+#####################################################################################
 
 #def main(argv):
 
@@ -28,9 +150,9 @@ for opt, arg in opts:
    elif opt in ("-t", "--ftype"):
       filetype = arg
 
-print ('inputfile is ', inputfile)
-print ('outputfile is ', outputfile)
-print('Type is', filetype)
+#print ('inputfile is ', inputfile)
+#print ('outputfile is ', outputfile)
+#print('Type is', filetype)
 
 
 """
@@ -41,55 +163,45 @@ if __name__ == "__main__":
 #
 # Construct Directory Paths for input and output files
 #
-finalInputPath = os.path.join("c:",os.sep, "Users","sdinn","OneDrive",inputfile)
+finalInputPath = os.path.join("c:",os.sep, "Users","sdinn","Downloads",inputfile)
 finalOutputPath = os.path.join("c:",os.sep, "Users","sdinn","Downloads",outputfile)
 
 print('FINAL Input PATH =', finalInputPath)
 print('FINAL Output PATH',finalOutputPath)
 
-
-#f = open(finalInputPath,'r')
-
-#f.close()
-
-
-
-
-#with open(r'c:\Users\sdinn\Downloads\MoneyDB181222.csv', 'r') as f:
+#
+# Open CSV file & process
+#
 with open(finalInputPath, 'r') as f:
 
     csv_reader = csv.reader(f)
     for line in csv_reader:
         # process each line
-        print(line)
+        #print(line)
+        if filetype == 'NW':
+           if(process_NW_csv_row(line) == NOT_PARSED):
+               print('Not Parsed:', line)
+        elif filetype == 'IG':
+            process_IG_csv_row(line)
 
-print(line[0])
 
-
-#workbook = xlsxwriter.Workbook(r'c:\Users\sdinn\Downloads\Example3.xlsx')
+########################################################################################
+################################   BUILD EXCEL #########################################
+########################################################################################
 
 workbook = xlsxwriter.Workbook(finalOutputPath)
 
 # By default worksheet names in the spreadsheet will be
 # Sheet1, Sheet2 etc., but we can also specify a name.
 worksheet = workbook.add_worksheet("My sheet")
-# Some data we want to write to the worksheet.
-scores = (
-   ['blah', 1000],
-   ['rahul', 100],
-   ['priya', 300],
-   ['harshita', 50],
-)
 
-# Start from the first cell. Rows and
-# columns are zero indexed.
-row = 0
-col = 0
+# Add a bold format to use to highlight cells.
+bold = workbook.add_format({'bold': 1})
 
-# Iterate over the data and write it out row by row.
-for name, score in (scores):
-   worksheet.write(row, col, name)
-   worksheet.write(row, col + 1, score)
-   row += 1
+if filetype == 'NW':
+    build_NW_excel()
+elif filetype == 'IG':
+    build_IG_excel()
+
 
 workbook.close()
